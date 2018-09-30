914aecf0c866c22df16cfe4808ea4336a334de72
Holilu <holilu2@gmail.com>
2018-09-30 20:40:36 +0200

    * Prvni JAVA program final verze

--- 001_HelloWorld/src/HelloWorld.java
+++ 001_HelloWorld/src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args){
 
-        /* Muj prvni JAVA program */
+        /* Muj prvni JAVA program vypisujici text do prikazove radky*/
         System.out.println("Hello World");
     }
 }
