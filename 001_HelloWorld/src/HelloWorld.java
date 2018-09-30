d7fff63f1ac76b12ca00ff12274b0d9237cec029
Holilu <holilu2@gmail.com>
2018-09-30 20:37:46 +0200

    * Prvni domaci ukol

--- 001_HelloWorld/src/HelloWorld.java
+++ 001_HelloWorld/src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args){
-        
+
         /* Muj prvni JAVA program */
         System.out.println("Hello World");
     }
---
+++ U01/src/U01.java
@@ -0,0 +1,7 @@
+public class U01 {
+
+    public static void main(String[] args){
+        /* Vypsani textu do prikazove radky */
+        System.out.println("Ahoj Tome. Jak se mas?");
+    }
+}
